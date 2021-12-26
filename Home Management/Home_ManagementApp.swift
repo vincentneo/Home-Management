6253cc1c123ad707ee87658616512165284a9e15
BaBaSaMa <19013345@myrp.edu.sg>
2021-12-26 20:18:54 +0800

    init commit

---
+++ Home Management/Home_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_ManagementApp.swift
+//  Home Management
+//
+//  Created by BaBaSaMa on 26/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Home_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
